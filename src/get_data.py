1f00399ec2426bbe04258e106f7b40a375d414df
Prajwal <prajwalgbdr03@gmail.com>
2023-06-06 20:14:01 +0530

    get data update

--- src/get_data.py
+++ src/get_data.py
@@ -7,23 +7,22 @@ import yaml
 import pandas as pd
 import argparse
 
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
 
+def read_params(config_path):
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+        return config
+    
 
 def get_data(config_path):
     config = read_params(config_path)
-    # print(config)
-    data_path = config['data_source']['s3_source']
+    data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=',', encoding='utf-8')
     return df
 
 
-if __name__=="__main__":
+if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default="params.yaml")
+    args.add_argument('--config', default='params.yaml')
     parsed_args = args.parse_args()
     get_data(config_path=parsed_args.config)
-    
